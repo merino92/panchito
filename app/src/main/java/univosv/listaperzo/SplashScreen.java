c8b00e2e21e560eecf72b6db88c01137c3031c81
Administrador <administrador@imac-de-administrador-2.local>
2017-08-28 20:09:46 -0600

    arreglo del splash arreglo del boton de inicio la lista y falta algunos detalles con el fab

--- app/src/main/java/univosv/listaperzo/SplashScreen.java
+++ app/src/main/java/univosv/listaperzo/SplashScreen.java
@@ -4,16 +4,26 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
-
+ProgressBar barra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        barra=(ProgressBar)findViewById(R.id.progressBar);
+        final int ProgresoTotal=100;
+        barra.setProgress(0);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                int Progreso=0;
+                while (Progreso<ProgresoTotal){
+                    Progreso +=25;
+                    barra.setProgress(Progreso);
+                }
+
 
                 Intent i = new Intent(SplashScreen.this,login.class);
                 startActivity(i);///////
