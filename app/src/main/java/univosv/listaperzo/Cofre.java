bf93274212bebf58e18dcfdee691e893cbec0201
Administrador <administrador@imac-de-administrador-2.local>
2017-08-30 10:24:35 -0600

    creando clases de share

---
+++ app/src/main/java/univosv/listaperzo/Cofre.java
@@ -0,0 +1,8 @@
+package univosv.listaperzo;
+
+/**
+ * Created by administrador on 30/8/17.
+ */
+
+public class Cofre {
+}
--- app/src/main/java/univosv/listaperzo/login.java
+++ app/src/main/java/univosv/listaperzo/login.java
@@ -1,6 +1,8 @@
 package univosv.listaperzo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,20 +14,26 @@ import android.widget.Toast;
 
 public class login extends AppCompatActivity {
   EditText contra,usuario;
+    public String  usuario1;
+    public final static String NOMBRE_SHARED_PREFERENCE="PerfilDatosTemporales";
+    public  SharedPreferences sharedPreferences=getApplicationContext().
+                                                    getSharedPreferences(NOMBRE_SHARED_PREFERENCE,MODE_PRIVATE);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
     public void entrar(View view){
         usuario=(EditText)findViewById(R.id.usuario);
         contra=(EditText)findViewById(R.id.clave);
-        String usuario1,contra1;
+        String contra1;
         usuario1=usuario.getText().toString();
         contra1=contra.getText().toString();
 
         if (usuario1.equals("panchito")&& contra1.equals("panchito"))
         {
+                GuardarUsuario();
             Intent intent=new Intent(this,MainActivity.class);
             startActivity(intent);
         }
@@ -34,4 +42,11 @@ public class login extends AppCompatActivity {
             Toast.makeText(this,"ERROR USUARIO O CONTRA INCORRECTA",Toast.LENGTH_LONG).show();
         }
     }
+    private void GuardarUsuario(){
+
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString("USUARIO",usuario1);
+        editor.commit();
+    }
+
 }
