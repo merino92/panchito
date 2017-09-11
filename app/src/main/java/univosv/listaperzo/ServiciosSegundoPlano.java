8f97965c276b15004fc7b46763d670ab405a18ae
Administrador <administrador@imac-de-administrador-2.local>
2017-09-11 09:54:03 -0600

    notificaciones lista

---
+++ app/src/main/java/univosv/listaperzo/ServiciosSegundoPlano.java
@@ -0,0 +1,22 @@
+package univosv.listaperzo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by administrador on 11/9/17.
+ */
+
+public class ServiciosSegundoPlano extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+
+
+}
