3c5a77ccefe3691cb8482b1a3a15aff94862c70c
rafaelzelayax@gmail.com <Luga7013>
2017-09-13 11:07:33 -0600

    Alarma y notificacion funcionando

--- app/src/main/java/univosv/listaperzo/Notificaciones/Alarma.java
+++ app/src/main/java/univosv/listaperzo/Notificaciones/Alarma.java
@@ -19,18 +19,20 @@ public class Alarma {
         AlarmManager alarmManager = (AlarmManager) contexto.getSystemService(Context.ALARM_SERVICE);
         Intent intento = new Intent(contexto,Notificacion.class);
         intento.putExtra(NOMBRE,INDEX);
-        PendingIntent intentoPendiente = PendingIntent.getService(contexto,INDEX,intento,PendingIntent.FLAG_UPDATE_CURRENT);
-        Calendar calendario = Calendar.getInstance();
-        calendario.add(calendario.MONTH,9);
-        calendario.add(calendario.DAY_OF_WEEK,3);
-        calendario.add(calendario.HOUR,9);
-        calendario.add(calendario.MINUTE,40);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP,calendario.getTimeInMillis(),intentoPendiente);
-        }
-        else{
-            alarmManager.set(AlarmManager.RTC_WAKEUP,calendario.getTimeInMillis(),intentoPendiente);
-        }
+
+            PendingIntent intentoPendiente = PendingIntent.getService(contexto, INDEX, intento, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Calendar calendario = Calendar.getInstance();
+        /*calendario.add(calendario.MONTH,9);
+        calendario.add(calendario.DAY_OF_WEEK,3);*/
+
+            calendario.add(calendario.MINUTE, INDEX);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendario.getTimeInMillis(), intentoPendiente);
+            } else {
+                alarmManager.set(AlarmManager.RTC_WAKEUP, calendario.getTimeInMillis(), intentoPendiente);
+            }
+
     }
 
 }
