1c72622760751c55f1a8636edc5a7835a2e35937
Administrador <administrador@imac-de-administrador-4.local>
2017-09-27 11:44:36 -0600

    ejecutando servicio en segundo plano

--- app/src/main/java/univosv/listaperzo/HorariosClases/FuncionesDeHorarios.java
+++ app/src/main/java/univosv/listaperzo/HorariosClases/FuncionesDeHorarios.java
@@ -1,5 +1,14 @@
 package univosv.listaperzo.HorariosClases;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.Calendar;
+
+import univosv.listaperzo.ServiciosSegundoPlano;
+
 /**
  * Created by administrador on 27/9/17.
  */
@@ -20,6 +29,28 @@ public class FuncionesDeHorarios {
 
 
 
+
+    }
+
+    public void verificarHora(Context context){
+
+        AlarmManager alarmMgr;
+        PendingIntent alarmIntent;
+
+        alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, ServiciosSegundoPlano.class);
+        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+// Poner alarma para comenzar a las 1:30 a.m.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 1);
+        calendar.set(Calendar.MINUTE, 30);
+
+// setRepeating() permite definir el intervalo de repetición
+        long unDia = 1000 * 60 * 60 * 24;
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                unDia, alarmIntent);
 
     }
 
--- app/src/main/java/univosv/listaperzo/ServiciosSegundoPlano.java
+++ app/src/main/java/univosv/listaperzo/ServiciosSegundoPlano.java
@@ -1,9 +1,14 @@
 package univosv.listaperzo;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -24,7 +29,7 @@ public class ServiciosSegundoPlano extends Service {
     }
     public void onCreate(){
         super.onCreate();
-        LanzarAvisos();
+
     }
     public void onStart(Intent intent, int startId){
         System.out.println("El servicio a Comenzado");
@@ -35,15 +40,24 @@ public class ServiciosSegundoPlano extends Service {
         System.out.println("El servicio a Terminado");
     }
 
-public void  LanzarAvisos(){
+    private void NotificacionPush(String materia,String aulayHora,int n ){
 
+        NotificationCompat.Builder builder;
+        NotificationManager AdNotificaciones=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-
-
-
-
-
-
-}
+        int icono = R.mipmap.logofab;
+        Intent intent = new Intent(this, Horarios.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,intent, 0);
+        builder = new NotificationCompat.Builder(this).
+                setContentIntent(pendingIntent).
+                setContentTitle(materia).
+                setContentText(aulayHora).
+                setSmallIcon(icono).
+                setVibrate(new long[] {100, 250, 100, 500}).
+                setAutoCancel(true);
+        //le agrega titutolo,despcripcion,el icono a la notificacion
+        AdNotificaciones.notify(n,builder.build());
+        //lanza la notificacion
+    }
 
 }
